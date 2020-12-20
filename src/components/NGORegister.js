1b8e65057ee7c9984d3c9d4f7c1f77781e647500
Prathmesh Khandelwal <201952225@iiitvadodara.ac.in>
2020-12-20 15:49:58 +0530

    everything done

--- src/components/NGORegister.js
+++ src/components/NGORegister.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 
 class NGORegister extends Component {
 
-  onLocationHandler = () => {
+
+  state= {
+    name:``,
+    email:``,
+    city:``,
+    pincode:0,
+    contact:null,
+    point:``,
+    temp:null 
+  }
+
+  onLocationHandler = (e) => {
+    e.preventDefault()
     navigator.geolocation.getCurrentPosition((position)=>{
       console.log(position)
+      this.setState({
+        point:`POINT(${position.coords.latitude} ${position.coords.longitude})`
+      })
     })
+  }
+
+  onSubmitHandler = (e) => {
+    e.preventDefault()
+    axios.post('https://anivior.herokuapp.com/auth/register/', this.state).then(res=>this.setState({temp:`NGO successfully added!`}))
+  }
+  
+  componentDidMount(){
+    
   }
 
   render() {
 
-
     return (
       <>
-        {/* <Navigation/> */}
         <div style={{display:'flex',
         flexFlow:'column',
         justifyContent:'center',
@@ -29,26 +51,35 @@ class NGORegister extends Component {
           </div>
           <div class="form-group">
             <label for="exampleInputEmail1">NGO Name</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Name"/>
+            <input type="text" class="form-control" placeholder="Name" onChange={(e)=>this.setState({name:e.target.value})}/>
             <small id="emailHelp" class="form-text text-muted">Please fill the name of your Ngo.</small>
           </div>
           <div class="form-group">
             <label for="exampleInputPassword1">PinCode</label>
-            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Pincode"/>
+            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Pincode" onChange={(e)=>this.setState({pincode:e.target.value})}/>
           </div>
           <div class="form-group">
             <label for="exampleInputPassword1">City</label>
-            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="City"/>
+            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="City" onChange={(e)=>this.setState({city:e.target.value})}/>
           </div>
           <div class="form-group">
             <label for="exampleInputEmail1">Email</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"/>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" onChange={(e)=>this.setState({email:e.target.value})}/>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Contact Number</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Number" onChange={(e)=>this.setState({contact:e.target.value})}/>
           </div>
           <button type="submit" class="btn btn-primary" onClick={this.onLocationHandler}>Location</button>
           <br/>
           <br/>
 
-          <center><button onClick={()=>this.props.history.push('/')} type="submit" class="btn btn-primary">Submit</button></center>
+          <center><button onClick={this.onSubmitHandler} type="submit" class="btn btn-primary">Submit</button></center>
+
+          <div>
+            {this.state.temp}
+          </div>
+
       </form>
       </div>
       </>
--- src/components/ReportAnimal.jsx
+++ src/components/ReportAnimal.jsx
@@ -1,22 +1,43 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 
 class ReportAnimal extends Component {
 
-    onLocationHandler = () => {
-        navigator.geolocation.getCurrentPosition((position)=>{
-          console.log(position)
-        })
-    }
+  state={
+    category:``,
+    image:null,
+    
+    longitude:null,
+    latitude:null,
+    temp:null
+  }
 
-    onSelectHandler = (e) => {
-        console.log(e.target.value)
-    }
+  onReportHandler = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+    const data = new FormData();
+    data.append('category',this.state.category)
+    data.append('image',this.state.image);
+    data.append('longitude',74.657936)
+    data.append('latitude',25.361928)
+    // console.log(this.state)
+    // console.log(data.get("image"))
+    axios.post('https://anivior.herokuapp.com/ngo/help/', data).then(res=>this.setState({
+      temp:`Successfully Reported`
+    }))
+  }
 
-
-    fileChangeHandler = (e) => {
-        console.log(e.target.files[0])
-    }
+  onLocationHandler = (e) => {
+    e.preventDefault()
+    navigator.geolocation.getCurrentPosition((position)=>{
+      console.log(position)
+      this.setState({
+        longitude:position.coords.longitude,
+        latitude:position.coords.latitude
+      })
+    })
+  }
 
 
   render() {
@@ -40,7 +61,7 @@ class ReportAnimal extends Component {
             <div class="input-group-prepend">
                 <label class="input-group-text" for="inputGroupSelect01">Select catogery</label>
             </div>
-            <select onChange={(e)=> this.onSelectHandler(e)} class="custom-select" id="inputGroupSelect01">
+            <select onChange={(e)=> {this.setState({category:e.target.value})}} class="custom-select" id="inputGroupSelect01">
                 <option selected value="dog">Choose...</option>
                 <option value="Dog">Dog</option>
                 <option value="Cat">Cat</option>
@@ -49,29 +70,25 @@ class ReportAnimal extends Component {
          </div>
          <div class="form-group">
             <label for="exampleInputPassword1">Image</label>
-            <input type="file" name="file" onChange={(e)=>this.fileChangeHandler(e)}/>
+            <input type="file" name="file" onChange={(e)=>this.setState({image:e.target.files[0]})}/>
           </div>
           <div class="form-group">
             <label for="exampleInputPassword1">Locality</label>
-            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Pincode"/>
+            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Text" />
           </div>
           <div class="form-group">
             <label for="exampleInputPassword1">City</label>
             <input type="text" class="form-control" id="exampleInputPassword1" placeholder="City"/>
           </div>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Contact Number</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"/>
-          </div>
+         
           <button type="submit" class="btn btn-primary" onClick={this.onLocationHandler}>Location</button>
           <br/>
           <br/>
-          <center><button onClick={()=>{
-              setTimeout(()=>{
-                  this.props.history.push('/')
-              },3000)
-          }} type="submit" class="btn btn-primary" >Report</button></center>
+          <center><button onClick={this.onReportHandler} type="submit" class="btn btn-primary" >Report</button></center>
       </form>
+      <div>
+        {this.state.temp}
+      </div>
       </div>
       </>
       
